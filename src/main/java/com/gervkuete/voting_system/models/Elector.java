77e0353ab204a818d561eb741bd02583d1feccc6
Gervais <lepetitgervais1@gmail.com>
2018-11-04 00:07:53 +0300

    Project initialization

--- src/main/java/com/gervkuete/voting_system/models/Elector.java
+++ src/main/java/com/gervkuete/voting_system/models/Elector.java
@@ -31,12 +31,14 @@ public class Elector extends User {
         this.voted = voted;
     }
 
+    Connection con = DatabaseConnection.getConnection();
+    PreparedStatement pstm = null;
+    ResultSet rs = null;
+
     // log into the system 
     @Override
     public boolean enter(String login, String password) {
-        Connection con = DatabaseConnection.getConnection();
-        PreparedStatement pstm = null;
-        ResultSet rs = null;
+
         String sql = "SELECT login, password FROM Elector WHERE login=? and password=?";
         boolean authorized = false;
         try {
@@ -48,11 +50,6 @@ public class Elector extends User {
             authorized = rs.next();
         } catch (SQLException ex) {
             Logger.getLogger(Elector.class.getName()).log(Level.SEVERE, null, ex);
-        } finally {
-            DatabaseConnection.closeResultSet(rs);
-            DatabaseConnection.closePrepareStatement(pstm);
-            DatabaseConnection.closeConnection(con);
-
         }
 
         return authorized;
@@ -67,8 +64,32 @@ public class Elector extends User {
             int yesOrNo = JOptionPane.showConfirmDialog(null, "Do you really wish to vote this candidate ?", "Confirm vote", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
             if (yesOrNo == 0) {
                 VotePage.selectedCandidate.addVoice();
-                JOptionPane.showMessageDialog(null, "Thank you for voting\n Good bye!", "Message", JOptionPane.INFORMATION_MESSAGE);
-                setVoted(true);
+                String name = this.getName();
+                int id = 0;
+                String sql = "SELECT Id FROM user_t WHERE name = ?";
+                try {
+                    pstm = con.prepareStatement(sql);
+                    pstm.setString(1, name);
+                    rs = pstm.executeQuery();
+                    if (rs.next()) {
+                        id = rs.getInt("Id");
+                    }
+                    
+                    String updateStatus = "UPDATE elector_t SET voted = " + true + "WHERE User_Id = ?";
+                    pstm = con.prepareStatement(updateStatus);
+                    pstm.setInt(1, id);
+                    pstm.executeUpdate();
+                    //if (count == 1) {
+                        JOptionPane.showMessageDialog(null, "Thank you for voting\n Good bye!", "Message", JOptionPane.INFORMATION_MESSAGE);
+                    //}
+                } catch (SQLException ex) {
+                    Logger.getLogger(Elector.class.getName()).log(Level.SEVERE, null, ex);
+                } finally {
+                    DatabaseConnection.closeResultSet(rs);
+                    DatabaseConnection.closePrepareStatement(pstm);
+                    DatabaseConnection.closeConnection(con);
+
+                }
 
             }
         }
